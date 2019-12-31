cbd3c9b14fa1730da3dfd492d9bdd45646dc164f
hugosepulvedaarriaza <38169855+hugosep@users.noreply.github.com>
2019-12-31 14:22:37 -0300

    Update server.py

--- actividad-1/server.py
+++ actividad-1/server.py
@@ -1,9 +1,3 @@
-# Copyright 2019 The gRPC Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the Lic
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
